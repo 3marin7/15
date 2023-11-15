2997c13c98a2591dc1182d07ca01e56c1811b8c2
3marin7 <92381445+3marin7@users.noreply.github.com>
2023-11-15 16:42:22 -0500

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    int resultado = 0;
+    cout << "Cuanto es 39+50? ";
+    cin >> resultado;
+    if(resultado == 39+50)
+    {
+        cout << "Respuesta Correcta. Felicitaciones!\n";
+    }
+    system("PAUSE");
+}
